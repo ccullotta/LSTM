d4fe82c9a1d6f1e240d3c025b31029931f3efe97
ccullotta <ccxbox2000@gmail.com>
2020-04-24 16:33:44 -0500

    added volume to the model, gained actual understanding of the input layer

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import pickle
 from coin import *
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Activation
 from keras.layers import LSTM
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
@@ -31,36 +31,61 @@ def openBin(fileName):
     with open(fileName, 'rb') as Y:
         return pickle.load(Y)
 
-def sigmize(num, big):
-    num = num/big
-    1/(1+math.exp(-num))
+def normalizeBtc(num, big, small):
+    num = (num - small)/(big-small)
+    # num = num/big
+    # 1/(1+math.exp(-num))
     return num
-end = openBin("bitcoin.dat")
-myBits = []
-for day in end["Time Series (Digital Currency Daily)"]:
-    myBits.append(float(end["Time Series (Digital Currency Daily)"][day]['4b. close (USD)']))
+# def normalizeVol(num)
+# end = openBin("bitcoin.dat")
+# print(end)
+# btcQuotes = []
+# VolumeOfDay = []
+# for day in end["Time Series (Digital Currency Daily)"]:
+#     btcQuotes.append(float(end["Time Series (Digital Currency Daily)"][day]['4b. close (USD)']))
+#     VolumeOfDay.append(float(end["Time Series (Digital Currency Daily)"][day]['5. volume']))
+# baseData = [btcQuotes, VolumeOfDay]
 
-big = float(max(myBits[:730]))
-print(big)
-data = [[[sigmize(myBits[i+j], big)] for i in range(30)] for j in range(700)]
-targei = [sigmize(myBits[30 + i], big) for i in range(700)]
+# writeToBin(baseData, "inputData.dat")
+
+baseData = openBin("inputData.dat")
+data = [
+    [
+        [
+            normalizeBtc(baseData[0][i+j], float(max(baseData[0][j:j+30])), float(min(baseData[0][j:j+30]))),
+            normalizeBtc(baseData[1][i+j], float(max(baseData[1][j:j+30])), float(min(baseData[1][j:j+30])))
+            ] for i in range(30)
+        ] for j in range(700) 
+
+        ]
+print(data[0][0])
+targei = []
+for i in range(700):
+    if baseData[0][30+i] > baseData[0][i-1]:
+        targei.append(1) 
+    else:
+        targei.append(0)
 
 
 
 npData = np.array(data, dtype=float)
 npTarget = np.array(targei, dtype=float)
 
-print(npData.shape) #shape is 700,30,1 meaning 700 samples of 30 sets of 1 datum 
+print(npData.shape) #shape is 700,30,2 meaning 700 samples of 30 sets of 2 data 
 x_train,x_test,y_train,y_test = train_test_split(npData, npTarget, test_size = 0.2, random_state = 4)
 model = Sequential()
-model.add(LSTM((1),batch_input_shape=(None,30,1),return_sequences=False))
-model.compile(loss="mean_absolute_error", optimizer='adam', metrics=['accuracy'])
+model.add(LSTM((16),batch_input_shape=(None,30,2),return_sequences=False))
+model.add(Activation('relu'))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+model.compile(loss="binary_crossentropy", optimizer='adam', metrics=['accuracy'])
 
 print(model.summary())
 
 print(x_test.shape)
 print(y_test.shape)
-history = model.fit(x_train, y_train, epochs=200, validation_data=(x_test,y_test))
+print(x_train)
+history = model.fit(x_train, y_train, epochs=100, validation_data=(x_test,y_test))
 print(history)
 results = model.predict(x_test)
 
